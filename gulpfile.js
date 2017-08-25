8c7199e751d0d01fe9b938c1ce9e07527943d5f6
Vin Wong <vinwong@juicyapphk.com>
2017-08-25 17:09:40 +0800

    add  watch to less, scss, js. Compile when content changed.

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,9 @@
 
 // Config
 var config = {
-    '/assets/main/less/common.less': '/assets/main/css/common.min.css',
-    '/assets/cms/less/common.less': '/assets/cms/css/common.min.css'
+    // Source location                  Compile to location
+    './assets/main/less/common.less': './assets/main/css/common.min.css',
+    './assets/cms/less/common.less': './assets/cms/css/common.min.css'
 };
 
 // Gulp
@@ -14,12 +15,64 @@ var gulp = require('gulp'),
     watch = require('gulp-watch'),
     sass = require('gulp-sass'),
     less = require('gulp-less'),
-    sourcemaps = require('gulp-sourcemaps')
-    uglify = require('gulp-uglify');
+    sourcemaps = require('gulp-sourcemaps'),
+    uglify = require('gulp-uglify'),
+    uglifycss = require('gulp-uglifycss'),
+    colors = require('colors');
 
+var getTimeTag = function() {
+    var date = new Date();
+    var zeroPadLeft = function(str) {
+        return String(str).length == 1 ? '0'+ str : str;
+    };
+    var time = zeroPadLeft(date.getHours()) +':'+
+                zeroPadLeft(date.getMinutes()) +':'+
+                zeroPadLeft(date.getSeconds());
+    return colors.white('[') + colors.grey(time) + colors.white(']');
+};
 
-gulp.task('default', function() {
-    // TODO
+var watchCompile = function(source, output) {
+    gulp.watch(source, (data)=>{
+        if (data.type == 'changed') {
+            var ext = source.split('.').pop();
+            console.log(getTimeTag() + ' '+ colors.magenta(source) +' changed, compile with '+ colors.cyan(ext) +' scheme.');
+            switch (ext) {
+                case 'less':
+                    gulp.src(source)
+                        .pipe(sourcemaps.init())
+                        .pipe(less())
+                        .pipe(uglifycss({uglyComments: true}))
+                        .pipe(sourcemaps.write('.'))
+                        .pipe(gulp.dest(output));
+                    break;
+                case 'scss':
+                case 'sass':
+                    gulp.src(source)
+                        .pipe(sourcemaps.init())
+                        .pipe(sass({outputStyle: 'compressed'}))
+                        .pipe(uglifycss({uglyComments: true}))
+                        .pipe(sourcemaps.write('.'))
+                        .pipe(gulp.dest(output));
+                    break;
+                case 'js':
+                    gulp.src(source)
+                        .pipe(uglify())
+                        .pipe(gulp.dest(output));
+                    break;
+            }
+        }
+    });
+};
+
+gulp.task('watch', function() {
+    // watch config file
+    for (var source in config) {
+        console.log(getTimeTag() + ' Watching '+ colors.magenta(source));
+        watchCompile(source, config[source]);
+    }
 });
 
-// TODO
+
+
+
+gulp.task('default', ['watch']);
