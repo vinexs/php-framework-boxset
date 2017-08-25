3154d97bad3b92efaa6083d18c367f03e53c3493
Vin Wong <vinwong@juicyapphk.com>
2017-08-25 18:08:34 +0800

    Use gulpjs plugin instead of 3th party plugin

--- gulpfile.js
+++ gulpfile.js
@@ -6,73 +6,60 @@
 // Config
 var config = {
     // Source location                  Compile to location
-    './assets/main/less/common.less': './assets/main/css/common.min.css',
-    './assets/cms/less/common.less': './assets/cms/css/common.min.css'
+    'assets/main/less/common.less': 'assets/main/css/common.min.css',
+    'assets/cms/less/common.less': 'assets/cms/css/common.min.css'
 };
 
-// Gulp
+// Gulp Tasks
 var gulp = require('gulp'),
-    watch = require('gulp-watch'),
+    gutil = require('gulp-util'),
+    watch = require('glob-watcher'),
     sass = require('gulp-sass'),
     less = require('gulp-less'),
     sourcemaps = require('gulp-sourcemaps'),
     uglify = require('gulp-uglify'),
-    uglifycss = require('gulp-uglifycss'),
-    colors = require('colors');
+    uglifycss = require('gulp-uglifycss');
 
-var getTimeTag = function() {
-    var date = new Date();
-    var zeroPadLeft = function(str) {
-        return String(str).length == 1 ? '0'+ str : str;
-    };
-    var time = zeroPadLeft(date.getHours()) +':'+
-                zeroPadLeft(date.getMinutes()) +':'+
-                zeroPadLeft(date.getSeconds());
-    return colors.white('[') + colors.grey(time) + colors.white(']');
-};
-
-var watchCompile = function(source, output) {
-    gulp.watch(source, (data)=>{
-        if (data.type == 'changed') {
-            var ext = source.split('.').pop();
-            console.log(getTimeTag() + ' '+ colors.magenta(source) +' changed, compile with '+ colors.cyan(ext) +' scheme.');
-            switch (ext) {
-                case 'less':
-                    gulp.src(source)
-                        .pipe(sourcemaps.init())
-                        .pipe(less())
-                        .pipe(uglifycss({uglyComments: true}))
-                        .pipe(sourcemaps.write('.'))
-                        .pipe(gulp.dest(output));
-                    break;
-                case 'scss':
-                case 'sass':
-                    gulp.src(source)
-                        .pipe(sourcemaps.init())
-                        .pipe(sass({outputStyle: 'compressed'}))
-                        .pipe(uglifycss({uglyComments: true}))
-                        .pipe(sourcemaps.write('.'))
-                        .pipe(gulp.dest(output));
-                    break;
-                case 'js':
-                    gulp.src(source)
-                        .pipe(uglify())
-                        .pipe(gulp.dest(output));
-                    break;
-            }
-        }
-    });
+var fileCompile = function(source, output) {
+    var ext = source.split('.').pop();
+    gutil.log(gutil.colors.magenta(source) +' changed, compile with '+ gutil.colors.cyan(ext) +' scheme.');
+    switch (ext) {
+        case 'less':
+            gulp.src(source)
+                .pipe(sourcemaps.init())
+                .pipe(less())
+                .pipe(uglifycss({uglyComments: true}))
+                .pipe(sourcemaps.write('.'))
+                .pipe(gulp.dest(output));
+            break;
+        case 'scss':
+        case 'sass':
+            gulp.src(source)
+                .pipe(sourcemaps.init())
+                .pipe(sass({outputStyle: 'compressed'}))
+                .pipe(uglifycss({uglyComments: true}))
+                .pipe(sourcemaps.write('.'))
+                .pipe(gulp.dest(output));
+            break;
+        case 'js':
+            gulp.src(source)
+                .pipe(uglify())
+                .pipe(gulp.dest(output));
+            break;
+    }
 };
 
 gulp.task('watch', function() {
-    // watch config file
+    var paths = [];
     for (var source in config) {
-        console.log(getTimeTag() + ' Watching '+ colors.magenta(source));
-        watchCompile(source, config[source]);
+        gutil.log('Watching '+ gutil.colors.magenta(source));
+        paths.push(source);
     }
+    var watcher = watch(paths);
+    watcher.on('change', function(path, stat) {
+        var output = config[path];
+        fileCompile(path, output);
+    });
 });
-
-
-
 
 gulp.task('default', ['watch']);
